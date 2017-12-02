4595a23f870964dfc6e88a25ced67ef4197c7c5a
Sadman Sakib Saumik <>
2017-12-03 05:15:03 +0600

    CLLocation and MKMapView implemented for authentication and set region area, back to user current location button(centermapbutton) implemented

--- Pixel-City/Controller/MapVC.swift
+++ Pixel-City/Controller/MapVC.swift
@@ -8,22 +8,47 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    var authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius:Double = 1000
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
+    
     }
     
     @IBAction func centerMapButtonTapped(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerToLocation()
+        }
     }
     
 }
 
 extension MapVC: MKMapViewDelegate {
+    func centerToLocation() {
+        guard let location = locationManager.location?.coordinate else {return}
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location, regionRadius*2, regionRadius*2)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
     
+}
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices(){
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            centerToLocation()
+            return
+        }
+    }
 }
 
